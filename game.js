35d14c5ea31a61c1620817489dae92897ffee522
Duursakh Battulga <410038004@edu.cbe.ab.ca>
2025-12-09 14:04:43 -0700

    started game

---
+++ game.js
@@ -0,0 +1,7 @@
+export function makeGame(p) {
+    return {
+        test() {
+            console.log("Test successful");
+        }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,9 +1,20 @@
-function setup() {
-  createCanvas(400, 400);
-}
+import { makeGame } from "./game.js"
 
-function draw() {
-  background(220);
-  fill("red");
-  circle(200, 200, 100);
-}
+new p5 ((p) => {
+  const game = makeGame(p);
+  p.preload = () => {
+    let image = p.loadImage("/MainMenu_Test.png")
+  }
+  p.frameRate(24);
+  p.setup = () => {
+    p.createCanvas(1920, 1080, document.getElementById("game"));
+  }
+  p.draw = () => {
+    p.clear();
+    p.rect(0, 0, 1920, 1080);
+    p.fill("red");
+    p.circle(860, 540, 50);
+    p.nofill();
+    p.image(image);
+  }
+})
